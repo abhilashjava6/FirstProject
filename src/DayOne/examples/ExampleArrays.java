cfad6cffebf784a75d87fef3659eb0ba4cd64437
Abhilash Bonala <abhilashjava6@gmail.com>
2018-04-19 01:12:52 -0400

    try

--- src/DayOne/examples/ExampleArrays.java
+++ src/DayOne/examples/ExampleArrays.java
@@ -3,7 +3,8 @@ package DayOne.examples;
 public class ExampleArrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println("a="+a);
 
 	}
 
